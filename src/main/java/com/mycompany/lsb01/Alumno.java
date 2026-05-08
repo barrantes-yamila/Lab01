add126d3d59d4a28dc5c080b903201f5e51edfff
barrantes-yamila <barrantesyamila@gmail.com>
2026-05-08 12:20:41 -0500

    primer commit

---
+++ src/main/java/com/mycompany/lsb01/Alumno.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.lsb01;
+
+/**
+ *
+ * @author Yamila
+ */
+public class Alumno {
+    private String nombre;
+    private String apellido;
+    private String tipoDoc;
+    private String dni;
+    private String carnetExtranjeria;
+    private String nivelSocioeconomico;
+    private double porcentajeBeca;
+    private double montoPensionFinal;
+}
